f4b7d557364ba82a7e96dbee369a75a62bfd8ca3
root <root@DESKTOP-HJC6HRF.localdomain>
2020-10-09 02:11:51 +0300

    1_add_treads

--- fi.c
+++ fi.c
@@ -4,19 +4,34 @@
 #include <sys/statvfs.h>
 #include <dirent.h>
 #include <string.h>
+#include <pthread.h>
 
+void *fileext(void *);
 
-void fileext(char *mnt_dir);
-
-float txt = 0, bin = 0, lib = 0;
+static double txt = 0, bin = 0, lib = 0;
 static int txtcount = 0, bincount = 0, libcount = 0;
+
+typedef struct {
+	int thread_id;
+	char mnt_dir[PATH_MAX];
+	int is_main;
+	int txt_n;
+	int bin_n;
+	int lib_n;
+} parameters_t;
+
+#define MAX_THREAD_NUMBER 20
 
 int main(void)
 {
 	struct mntent *ent;
 	struct statvfs buff;
 	FILE *aFile;
+	pthread_t threads[MAX_THREAD_NUMBER] = {};
+	parameters_t thread_params[MAX_THREAD_NUMBER] = {};
 	aFile = setmntent("/etc/mtab", "r");
+	int i = 0;
+
 	if (aFile == NULL)
 	{
 		perror("setmntent");
@@ -24,7 +39,6 @@ int main(void)
 	}
 	
 	printf("Filesystem\t Size \t Used \t Avail \tMounted on\n");
-	
        
 	while (NULL != (ent = getmntent(aFile)))
 	{
@@ -40,73 +54,95 @@ int main(void)
 		printf("%ld \t\t",  ((buff.f_blocks * buff.f_frsize) / 1024) - ((buff.f_bavail * buff.f_frsize) / 1024));
 		printf("%ld \t\t", (buff.f_bavail * buff.f_frsize) / 1024);
 		printf("%s \n", ent->mnt_dir);
-		fileext(ent->mnt_dir);
-		
-	
-//	printf("*text documents -%d", txtcount);
-	}	
+	//	fileext("/root/filesystem/df");
+		if (i >= MAX_THREAD_NUMBER)
+		{
+		         perror("fail to create thread\n");
+//			fileext((void*)ent->mnt_dir);
+			 parameters_t p_main;
+			 p_main.thread_id = -1;
+			 strcpy(p_main.mnt_dir, ent->mnt_dir);
+			 p_main.is_main = 1;
+			 fileext(&p_main);
+		}
+		else
+		{
+			thread_params[i].thread_id = i;
+			strcpy(thread_params[i].mnt_dir, ent->mnt_dir);
+			thread_params[i].is_main = 1;
+			int pthc_ret = pthread_create(&threads[i], NULL, fileext, &thread_params[i]) ;
+			if (pthc_ret)
+			{
+				perror("fail to create thread\n");
+				//return
+			}
+		}
+		i++;
+	}
+
+	// wait until all threads will be finished
+	for (int k = 0; k <= i; k++)
+		pthread_join(threads[k], NULL);
+
+	txt = (txtcount / (double)(txtcount + bincount + libcount)) * 100;
+	bin = (bincount / (double)(txtcount + bincount + libcount)) * 100;
+	lib = (libcount / (double)(txtcount + bincount + libcount)) * 100;
+
+	printf("*text documents %f%%\n*binary files %f%%\n*libraries(so,a) %f%%\n",txt ,bin, lib);	
 	endmntent(aFile);
+
 }
-void fileext(char *mnt_dir)
+
+void *fileext(void *param)
 {
+	parameters_t *ctx = param;	
 	DIR *p;
 	struct dirent *pp;
-	p = opendir (mnt_dir);
+	int exit_in_the_end = 0;
+
+	if (ctx->is_main)
+	{
+		exit_in_the_end = 1;
+		ctx->is_main = 0;
+	}
+
+	p = opendir (ctx->mnt_dir);
 	if (p != NULL)
 	{
 		while ((pp = readdir (p))!=NULL)
 		{
+			int length = strlen(pp->d_name);
 			if (pp->d_type == DT_DIR && (strcmp(pp->d_name, ".") && 
 				strcmp(pp->d_name, "..")))
 			{
-#if 0
-				int path_len = strlen(mnt_dir);
-				int file_name_len = strlen(pp->d_name);
-				/* +1 for null symbol */
-				int new_path_len = path_len + file_name_len + 1;
-#endif
+				char temp[PATH_MAX];
 
-				int len = snprintf(NULL, 0, "%s%s%s", mnt_dir, "/", pp->d_name);
+				strcpy(temp, ctx->mnt_dir);
+				sprintf(ctx->mnt_dir, "%s%s%s", ctx->mnt_dir, "/", pp->d_name);
 
-				char *new_path = (char*)calloc(1, sizeof(char) * (len + 1));
-
-				sprintf(new_path, "%s%s%s", mnt_dir, "/", pp->d_name);
-
-				printf("THE FILE IS A DIRECTORY, path is: %s\n", new_path);
-#if 0
-				strncpy(new_path, mnt_dir, path_len);
-				printf("step 1:, path is: %s\n", new_path);
-				strncpy(new_path + path_len, pp->d_name, strlen(pp->d_name));
-
-				printf("THE FILE IS A DIRECTORY, path is: %s\n", new_path);
-#endif
-				fileext(new_path);
-
-				free(new_path);
+				fileext(ctx);
+				strcpy(ctx->mnt_dir, temp);
 			}
 			
-			int length = strlen(pp->d_name);
 			if (strncmp(pp->d_name + length - 4, ".txt", 4) == 0)
-				++txtcount;
-				printf("%s\n",pp->d_name);
+				++ctx->txt_n;
 			if (strncmp(pp->d_name + length - 4, ".bin", 4) == 0)
-				++bincount;
-				printf("%s\n",pp->d_name);
+				++ctx->bin_n;
 			if (strncmp(pp->d_name + length - 3, ".so", 3) == 0)
-				++libcount;
-				printf("%s\n",pp->d_name);
+				++ctx->lib_n;
 			if (strncmp(pp->d_name + length - 2, ".a", 2) == 0)
-			        ++libcount;
-				printf("%s\n",pp->d_name);
+			        ++ctx->lib_n;
 		}
-	
-	(void) closedir (p);
-	
-//	txt = (txtcount / (txtcount + bincount + libcount)) * 100;
-//	bin = (bincount / (txtcount + bincount + libcount)) * 100;
-//	lib = (libcount / (txtcount + bincount + libcount)) * 100;
-	
+
+	closedir (p);
 	}
 
-}
+	if (exit_in_the_end)
+	{
+		libcount += ctx->lib_n;
+		bincount += ctx->bin_n;
+		txtcount += ctx->txt_n;
 
+		pthread_exit(NULL);
+	}
+}
